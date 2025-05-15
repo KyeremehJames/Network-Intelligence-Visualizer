46dfb2e776bcb99f2e1a9bd22b921cd28aa40115
James Kyeremeh <104057794+KyeremehJames@users.noreply.github.com>
2025-05-15 08:22:08 -0700

    Update networkintelligencevisualizer.py

--- Network Intelligence Visualizer/networkintelligencevisualizer.py
+++ Network Intelligence Visualizer/networkintelligencevisualizer.py
@@ -1,299 +1,180 @@
-import sys 
-import pygeoip
+#!/usr/bin/env python3
+"""
+Threat Intelligence Visualizer
+Author: [Your Name]
+Date: [Current Date]
+"""
+
+import sys
+import logging
+import yaml
 import dpkt
 import socket
-import time
-import webbrowser
+from pathlib import Path
+import pygeoip
+import requests
+from typing import Dict, List, Optional
 
-# These dictonaries are used for the key value pairs. Keys being the IP addresses and the values being the lat-long position
-abc={}
-dest_abc={}
+# Configuration
+CONFIG_FILE = "config/config.yaml"
+LOG_FILE = "threatviz.log"
 
-black_listed_ip=['217.168.1.2','192.37.115.0','212.242.33.35','147.137.21.94']
+class ThreatIntelAnalyzer:
+    def __init__(self):
+        self.config = self._load_config()
+        self.geoip_db = self._init_geoip()
+        self.logger = self._init_logging()
+        self.threat_feeds = self.config.get('threat_feeds', [])
+        
+    def _load_config(self) -> Dict:
+        """Load configuration from YAML file"""
+        try:
+            with open(CONFIG_FILE) as f:
+                return yaml.safe_load(f)
+        except Exception as e:
+            print(f"Error loading config: {e}")
+            sys.exit(1)
 
-# This dictionary is used for keeping the record of authorized users.
+    def _init_geoip(self) -> pygeoip.GeoIP:
+        """Initialize GeoIP database"""
+        db_path = Path(self.config.get('geoip_db_path', 'data/GeoLiteCity.dat'))
+        if not db_path.exists():
+            raise FileNotFoundError(f"GeoIP database not found at {db_path}")
+        return pygeoip.GeoIP(str(db_path))
 
-auth_users={"root":"root","soumil":"soumil"}
+    def _init_logging(self):
+        """Configure logging system"""
+        logging.basicConfig(
+            filename=LOG_FILE,
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+        return logging.getLogger()
 
+    def check_threat_feeds(self, ip: str) -> List[str]:
+        """Check IP against multiple threat intelligence feeds"""
+        threats = []
+        
+        # Local blacklist check
+        if ip in self.config.get('blacklists', {}).get('local', []):
+            threats.append("local_blacklist")
+            
+        # Dynamic threat feed checks
+        for feed in self.threat_feeds:
+            if feed['type'] == 'abuseipdb':
+                if self._check_abuseipdb(ip, feed['api_key']):
+                    threats.append("abuseipdb")
+                    
+        return threats
 
-"""
-This function is used for generating information from the IP addresses. It uses the GeoLiteCity data base for doing this .
-'gic.record_by_addr()' return a dictonary that has all the parameters of the corresponding IP address. We can print any of them.
-"""
-def geoip_city(string):
-	if string in black_listed_ip:	
-		path='/home/soumil/build/geoip/GeoLiteCity.dat'
-		gic=pygeoip.GeoIP(path)
-		#print gic
-		try:	
-			a=gic.record_by_addr(string)
-			#print a
-			pcity=a['city']
-			pregion=a['region_code']
-			pcountry=a['country_name']
-			plong=a['latitude']
-			plat=a['longitude']
-			print  "\n"	
-			print '[*] Target : ' + string + ' Geo Located .'
-			print " \n"	
-			print '[+] City: '+ str(pcity) + ', Region : '+ str(pregion) + ', Country: ' + str(pcountry)
-			print "\n"	
-			print '[+] Latitude : ' +  str(plat) + ', Longitude : ' + str(plong)
-			print "\n"
-		except:
-			print " \n ********** IP Unregistered **********"
-	else:
-		pass
+    def _check_abuseipdb(self, ip: str, api_key: str) -> bool:
+        """Check IP against AbuseIPDB API"""
+        try:
+            url = f"https://api.abuseipdb.com/api/v2/check?ipAddress={ip}"
+            headers = {'Key': api_key, 'Accept': 'application/json'}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                data = response.json()
+                return data['data']['abuseConfidenceScore'] > 75
+        except Exception as e:
+            self.logger.error(f"AbuseIPDB check failed: {e}")
+        return False
 
+    def analyze_pcap(self, pcap_path: str, output_format: str = "cli"):
+        """Main analysis function"""
+        try:
+            with open(pcap_path, 'rb') as f:
+                pcap = dpkt.pcap.Reader(f)
+                
+                if output_format == "kml":
+                    print(self._generate_kml_header())
+                
+                for ts, buf in pcap:
+                    try:
+                        eth = dpkt.ethernet.Ethernet(buf)
+                        ip = eth.data
+                        src_ip = socket.inet_ntoa(ip.src)
+                        dst_ip = socket.inet_ntoa(ip.dst)
+                        
+                        self._process_ip(src_ip, "Source", output_format)
+                        self._process_ip(dst_ip, "Destination", output_format)
+                        
+                    except Exception as e:
+                        self.logger.debug(f"Packet processing error: {e}")
+                        
+                if output_format == "kml":
+                    print(self._generate_kml_footer())
+                    
+        except FileNotFoundError:
+            self.logger.error(f"PCAP file not found: {pcap_path}")
+            sys.exit(1)
 
+    def _process_ip(self, ip: str, ip_type: str, output_format: str):
+        """Process individual IP address"""
+        threats = self.check_threat_feeds(ip)
+        if not threats:
+            return
+            
+        geo = self.geoip_db.record_by_addr(ip)
+        if not geo:
+            self.logger.warning(f"Could not geolocate IP: {ip}")
+            return
+            
+        if output_format == "cli":
+            self._print_cli_report(ip, ip_type, threats, geo)
+        else:
+            self._generate_kml_placemark(ip, ip_type, geo)
 
-"""
-We are using this function to generate latitudes and longitudes an IP address from the GeoLiteCity database. We will be using these Latitudes 
-and longitudes fro plotting placemarks on google maps (my maps). This function is used for Source IP addresses.
-"""
+    def _print_cli_report(self, ip: str, ip_type: str, threats: List[str], geo: Dict):
+        """Generate CLI output"""
+        print(f"\n[THREAT] {ip_type} IP: {ip}")
+        print(f"Threat Indicators: {', '.join(threats)}")
+        print(f"Location: {geo.get('city', 'Unknown')}, {geo.get('country_name', 'Unknown')}")
+        print(f"Coordinates: {geo.get('latitude')}, {geo.get('longitude')}")
+        print("-" * 50)
 
-def kml_geoip_city(string):
-	if string in black_listed_ip:
-	
-		path='/home/soumil/build/geoip/GeoLiteCity.dat'
-		gic=pygeoip.GeoIP(path)
+    def _generate_kml_header(self) -> str:
+        """Generate KML document header"""
+        return """<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+<Document>
+  <name>Threat Intelligence Visualization</name>
+  <open>1</open>
+  <Style id="threatStyle">
+    <LabelStyle>
+      <color>ff0000ff</color>
+    </LabelStyle>
+  </Style>"""
 
-		try:	
-			a=gic.record_by_addr(string)
-			pcity=a['city']
-			plong=a['latitude']
-			plat=a['longitude']
-			abc[str(string)]=str(plat)+","+str(plong)
-			# Here we are inputing the IP:lat,long to the source Dictionary defined earlier.
-		
-		except:
-			pass
-	else:
-		pass
-
-
-	
-"""
-We are using this function to generate latitudes and longitudes an IP address from the GeoLiteCity database. We will be using these Latitudes 
-and longitudes fro plotting placemarks on google maps (my maps). This function is used for Destination IP addresses.
-"""
-
-def kml_dest_geoip_city(string):
-	if string in black_listed_ip:
-		path='/home/soumil/build/geoip/GeoLiteCity.dat'
-		gic=pygeoip.GeoIP(path)
-
-		try:	
-			a=gic.record_by_addr(string)
-		
-			pcity=a['city']
-			plong=a['latitude']
-			plat=a['longitude']
-			dest_abc[str(string)]=str(plat)+","+str(plong)
-			# Here we are inputing the IP:lat,long to the destination dictionary defined earlier.
-		
-		except:
-
-			pass
-	else:
-		pass
-
-
-
-"""
-The printcap function is used for printing information related to a particular IP address. This function takes in a pcap. 
-It uses the dptk module in python to parse the pcap to find out all the source IP and destination IP of all the packets.
-It now prints all the information on the screen in text format.
-"""
-	
-	
-def printpcap(pcap):
-	for (ts,buf) in pcap:
-		try:
-			eth=dpkt.ethernet.Ethernet(buf)
-			ip=eth.data
-			src=socket.inet_ntoa(ip.src)
-			dst=socket.inet_ntoa(ip.dst)
-			if src in black_listed_ip:
-			
-				print "-------------------------------------------------------------------------------------------------"
-				print '[+] Source IP: '+str(src)+ '------->  Destination IP: '+ str(dst)
-				print "Source IP Information:"			
-				print geoip_city(str(src))
-			elif dst in black_listed_ip:
-			
-				print "=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-"
-				print "Destination IP Information:"
-				print geoip_city(str(dst))
-				print "--------------------------------------------------------------------------------------------------"
-			else:
-				pass
-
-		except:
-			pass
-
-"""
-The view_google function is some what similar to the printpcap function that is written above.
-Even this function takes in a pcap, parses it using 'dpkt' and then sends the source and dest ip addresses
-to the fnction that generates kml format for the data so that it can be used to represet on a graph.
-"""
-def view_google(pcap):	
-	
-	for (ts,buf) in pcap:
-		try:
-			eth=dpkt.ethernet.Ethernet(buf)
-			ip=eth.data
-			src=socket.inet_ntoa(ip.src)
-			dst=socket.inet_ntoa(ip.dst)
-			kml_geoip_city(str(src))
-			kml_dest_geoip_city(str(dst))
-			
-		except:
-			pass
-
-"""
-The print_placemarks_in_kml(abc) funtion is used for generating the kml format of the data for the representation of the 
-google maps. It takes in a dictionary  that contains key-value pairs (IP:lat,long). And inturn generate the kml format 
-of the data. This function is used for generating kml for Source IP addresses.
-"""
-
-def print_placemarks_in_kml(abc):
-	for i in abc.keys():
-		print """
+    def _generate_kml_placemark(self, ip: str, ip_type: str, geo: Dict):
+        """Generate KML placemark for a single IP"""
+        coords = f"{geo.get('longitude')},{geo.get('latitude')}"
+        print(f"""
   <Placemark>
-    <name> SOURCE IP Address: %s</name>
-    <styleUrl>#exampleStyleDocument</styleUrl>
+    <name>{ip_type} IP: {ip}</name>
+    <description>
+      City: {geo.get('city', 'Unknown')}
+      Country: {geo.get('country_name', 'Unknown')}
+    </description>
+    <styleUrl>#threatStyle</styleUrl>
     <Point>
-      <coordinates>%s</coordinates>
+      <coordinates>{coords}</coordinates>
     </Point>
-  </Placemark>
-"""%(i,abc[i])
+  </Placemark>""")
 
+    def _generate_kml_footer(self) -> str:
+        """Generate KML document footer"""
+        return """
+</Document>
+</kml>"""
 
-
-"""
-The print_dest_placemarks_in_kml(dest_abc) funtion is used for generating the kml format of the data for  the representation of the 
-google maps. It takes in a dictionary  that contains key-value pairs (IP:lat,long). And inturn generate the kml format 
-of the data. This function is used for generating kml for destication IP addresses.
-"""
-
-def print_dest_placemarks_in_kml(dest_abc):
-	for i in dest_abc.keys():
-		print """
-  <Placemark>
-    <name> DESTINATION IP Address: %s</name>
-    <styleUrl>#exampleStyleDocument</styleUrl>
-    <Point>
-      <coordinates>%s</coordinates>
-    </Point>
-  </Placemark>
-"""%(i,dest_abc[i])
-	
-
-
-
-"""
-Now the main function starts : 
-"""
-
-"""
-The first try-catch block is used to validate if the user is authorized to use this tool. If the username entered by the user is present in 
-our record (i.e. the dictionary"auth_users"), he/she gets to use the tool. Else One cannot access the tool. This can be considered as one of 
-the security feature of our tool.
-"""
-
-if len(sys.argv)<2:
-	print "\n -------------------- Please enter the required arguments------------------------- "
-	print "\n Correct syntax is : <FileName>.py username password cli/kml\n"
-	print "\ncli- stands for Command Line Output"
-	print "\nkml- stands fro a KML output which is required for visualization using a Google Map"
-
-else:
-
-	try:
-		if str(sys.argv[1]) in auth_users:
-
-			"""
-			The second try-catch block is used to validate the password entered by the user to the corresponding entered username. Once the username is 
-			validated, we must validate the password as well. This is used fro securit hardening of the tool.
-			"""
-
-			try:
-				if str(sys.argv[2])==auth_users[str(sys.argv[1])]:
-
-					"""
-					The user must specify what type of output does he/she desire. If he/she desires an output on the command line or a textual output
-					he should use "cli" option.
-					If he/she desires to use maps for visualizaing the contents of the analysis. There is a need for a KML file that must 
-					be uploaded to the google maps webiste that opens an the end.
-					For Visualizing it output on the maps. He/She must redirect the output of the program using pipes to a ".kml" file. 
-					This .kml file is then uploded on the mymaps website.
-					"""
-					try:
-	
-						if str(sys.argv[3])=="cli":
-							f=open('/home/soumil/Downloads/fuzz-2006-06-26-2594.pcap')
-							pcap=dpkt.pcap.Reader(f)
-							printpcap(pcap)
-							f.close()
-							sys.exit(1)
-						elif str(sys.argv[3])=="kml":
-		
-							print """
-						<?xml version="1.0" encoding="UTF-8"?>
-						<kml xmlns="http://www.opengis.net/kml/2.2">
-						<Document>
-						  <name>sourceip.kml</name>
-						  <open>1</open>
-						  <Style id="exampleStyleDocument">
-						    <LabelStyle>
-						      <color>ff0000cc</color>
-						    </LabelStyle>
-						  </Style>\n"""	
-							f=open('/home/soumil/Downloads/fuzz-2006-06-26-2594.pcap')
-							pcap=dpkt.pcap.Reader(f)
-							view_google(pcap)
-							f.close()
-							print_dest_placemarks_in_kml(dest_abc)
-							print_placemarks_in_kml(abc)
-							#print abc
-							print """\n
-						</Document>
-						</kml>
-						"""
-							new=1
-							url="https://www.google.com/maps/d/splash?app=mp"
-							webbrowser.open(url,new=new)
-						
-
-
-						else:
-							raise exception
-						
-
-					except:
-						print "\nYou Entered a worng option. Or may be your Syntax is wrong"
-						print "\n Correct syntax is : <FileName>.py username password cli/kml\n"
-						print "\ncli- stands for Command Line Output"
-						print "\nkml- stands fro a KML output which is required for visualization using a Google Map"
-				else:
-					raise exception
-				
-
-			except:
-				print "\n The PASSWORD you entered is NOT CORRECT !!!!!! "
-				print "\n Correct syntax is : <FileName>.py username password cli/kml\n"
-				print "\ncli- stands for Command Line Output"
-				print "\nkml- stands fro a KML output which is required for visualization using a Google Map"
-
-		else:
-
-			raise exception
-
-
-	except:
-
-		print "\n Sorry %s. You are NOT AUTHORIZED to use this tool!!!!!!!!!!!"%str(sys.argv[1])
-		print "\n Correct syntax is : <FileName>.py username password cli/kml\n"
-		print "\ncli- stands for Command Line Output"
-		print "\nkml- stands fro a KML output which is required for visualization using a Google Map"
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python threatviz.py <pcap_file> [cli|kml]")
+        sys.exit(1)
+        
+    output_format = sys.argv[2] if len(sys.argv) > 2 else "cli"
+    pcap_file = sys.argv[1]
+    
+    analyzer = ThreatIntelAnalyzer()
+    analyzer.analyze_pcap(pcap_file, output_format)
